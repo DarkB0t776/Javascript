46ae1b9dc79e327280e6dd65ec9ff138c1c6afcb
Dima <dima23790@gmail.com>
2020-03-05 16:55:53 +0200

    Added ships

--- battleship/script.js
+++ battleship/script.js
@@ -4,9 +4,10 @@ const hitsNumber = document.getElementById('hit');
 const shipsSunk = document.getElementById('dead');
 const enemy = document.getElementById('enemy');
 const again = document.getElementById('again');
+const title = document.querySelector('.header');
 
 const play = {
-  record: 0,
+  record: localStorage.getItem('battleShipRecord') || 0,
   shotsNumber: 0,
   hitsNumber: 0,
   shipsSunk: 0,
@@ -22,12 +23,39 @@ const play = {
   }
 };
 
+const game = {
+  ships: [
+    {
+      location: ['26', '36', '46', '56'],
+      hit: ['','','','']
+    },
+    {
+      location: ['11','12', '13'],
+      hit: ['','', '']
+    },
+    {
+      location: ['69', '79'],
+      hit: ['', '']
+    },
+    {
+      location: ['32'],
+      hit: ['']
+    }
+  ],
+  shipCount: 4
+};
+
 const show = {
-  hit(){},
+  hit(target){
+    this.changeClass(target, 'hit');
+  },
   miss(target){
     this.changeClass(target, 'miss');
   },
-  dead(){},
+  dead(id){
+    const target = document.getElementById(id);
+    this.changeClass(target, 'dead');
+  },
   changeClass(target, value){
     target.className = value;
   }
@@ -36,15 +64,51 @@ const show = {
 const fire = (event) => {
   const target = event.target;
   const classes = ['miss', 'hit', 'dead'];
-  if(classes.includes(target.className)){
+  if(classes.includes(target.className) || target.tagName !== 'TD'){
     return;
   }
   show.miss(target);
   play.updateData = 'shotsNumber';
+
+  for(let i = 0; i < game.ships.length; i++) {
+    const ship = game.ships[i];
+    const index = ship.location.indexOf(target.id);
+    if(index >= 0){
+      show.hit(target);
+      play.updateData = 'hitsNumber';
+      ship.hit[index] = 'x';
+      const life = ship.hit.indexOf('');
+      if(life < 0){
+        play.updateData = 'shipsSunk';
+        for(const cell of ship.location){
+          show.dead(cell);
+        }
+
+        game.shipCount -= 1;
+
+        if(game.shipCount < 1){
+          title.textContent = 'Game is Over!';
+          title.style.color = 'red';
+          if(play.shotsNumber < play.record || play.record === 0){
+            localStorage.setItem('battleShipRecord', play.shotsNumber);
+            play.record = play.shotsNumber;
+            play.render();
+          }
+
+          enemy.removeEventListener('click', fire);
+        }
+      }
+    }
+  }
 };
 
 const init = () => {
   enemy.addEventListener('click', fire);
+  play.render();
+
+  again.addEventListener('click', () => {
+    location.reload();
+  });
 };
 
 init();
